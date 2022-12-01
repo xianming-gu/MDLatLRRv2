2d4ca8bd7bae528a9d40dad61784ac6ef3360702
unknown <xianming_gu@foxmail.com>
2022-12-01 20:34:17 +0800

    first commit

---
+++ main.m
@@ -0,0 +1,24 @@
+%主函数
+clear
+clc
+
+disp('----------main------------');
+% load matrices
+load('./models/MD_L_16_detail_mix_2000_s1000.mat');
+load('./models/MD_P_16_detail_mix_2000_s1000.mat');
+L = L_16;
+P = P_16;
+
+%根据融合图像类型进行融合
+% fusion_all_kinds( 'CT_MR', P, L )
+% fusion_all_kinds( 'MR_PET', P, L )
+% fusion_all_kinds( 'MR_SPECT', P, L )
+% fusion_single_channel( 'CT-MRI', P, L )
+% fusion_all_kinds( 'PET-MRI', P, L )
+fusion_all_kinds( 'SPECT-MRI', P, L )
+
+
+
+
+
+
